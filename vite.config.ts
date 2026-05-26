48b56a4581690eb851e6b1d1ad4639bd09144060
FactOK <147402799+FactNJS1014@users.noreply.github.com>
2026-05-26 11:46:44 +0700

    add config

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,33 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/",
+
+  build: {
+    chunkSizeWarningLimit: 1000,
+
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (
+              id.includes("react") ||
+              id.includes("react-dom") ||
+              id.includes("react-router-dom")
+            ) {
+              return "vendor";
+            }
+
+            if (id.includes("recharts")) {
+              return "charts";
+            }
+          }
+        },
+      },
+    },
+  },
 });
